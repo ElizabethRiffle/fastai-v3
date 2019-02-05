0611f316035b6d7beb165e9bbb5c1c03eee5fab0
ElizabethRiffle <elizabeth@wa-inc.com>
2019-02-04 21:52:43 -0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -8,10 +8,10 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1ixJdEhzkbuCsGKA7fR4URXgY_8k38fdy'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Sneakers','Sweatshirts','Sunglasses']
 path = Path(__file__).parent
 
 app = Starlette()
